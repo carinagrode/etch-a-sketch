eb059d64c4f523939e50efb1c7219571f2a8c986
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-07-02 13:23:15 +0200

    Add design to the button and website

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ for (let i = 0; i < 256; i++) {
     });
 }
 
+const newButton = document.getElementById('newbutton');
+newButton.addEventListener('click', createNewGrid);
+
+function createNewGrid() {
+    const newWidth = getNewGridWidth();
+    
+
+    
+
+
+}
+
+function getNewGridWidth() {
+    const newGridWidth = prompt('How many squares do you want your new canvas to have per side?', '60');
+
+    if (newGridWidth < 3 || newGridWidth > 100) {
+        const fixedNewGridWidth = prompt ("Let's choose a number between 3 and 100 for a nice canvas :)", "70");
+        return fixedNewGridWidth;
+    } else {
+        return newGridWidth;
+    }
+}
